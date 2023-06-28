ad9df26c2138fafd7e933adb13b002e4cff43d2e
vikrantgautam <vikrantgautam947@gmail.com>
2023-06-28 21:46:34 +0530

    added user apis

--- just-app-backend/routes/user.routes.js
+++ just-app-backend/routes/user.routes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const UserController = require("../controllers/user.controller");
 
 router.post("/signUp", UserController.signUp);
+router.get("/allUsers", UserController.allUsers);
+router.get("/:id", UserController.viewUser);
+router.put("/:id", UserController.updateUser);
+router.delete("/:id", UserController.deleteUser);
 
 module.exports = router;
--- just-app-backend/server.js
+++ just-app-backend/server.js
@@ -8,10 +8,22 @@ const port = process.env.PORT || 5000;
 
 //app.use(bodyParser.json()); // for parsing application/json
 //app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PATCH,PUT,DELETE,OPTIONS"
+  );
+  next();
+});
 app.use(express.json());
 
 const userRoute = require("./routes/user.routes.js");
-app.use("/user", userRoute);
+app.use("/api/user", userRoute);
 
 connectDB(); // connect to mongodb
 
